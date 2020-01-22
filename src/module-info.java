46ef52613516bdc76e66420d86169fbe57372d5d
calozanog-uniandes <calozanog@uniandes.edu.co>
2020-01-22 14:17:49 -0500

    project
    Comentario sobre la actualizacion registrada en repositorio local

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module T0_202010 {
+	exports test.data_structures;
+	exports controller;
+	exports view;
+	exports test.logic;
+	exports model.logic;
+	exports main;
+	exports model.data_structures;
+}
